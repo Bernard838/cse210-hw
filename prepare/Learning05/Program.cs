929e802abf448f3cbc0343bb08022b358e721741
Bernard838 <adjeibernard899@gmail.com>
2024-12-15 22:46:18 -0800

    Learning05 Assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Test Assignment class
+        Assignment assignment = new Assignment("Samuel Mckline", "Multiplication");
+        Console.WriteLine(assignment.GetSummary()); // Output: Samuel Bennett - Multiplication
+
+        // Test MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Roberto Yoon", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());       // Output: Roberto Rodriguez - Fractions
+        Console.WriteLine(mathAssignment.GetHomeworkList());  // Output: Section 7.3 Problems 8-19
+
+        // Test WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Benedicta Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());        // Output: Mary Waters - European History
+        Console.WriteLine(writingAssignment.GetWritingInformation()); // Output: The Causes of World War II by Mary Waters
     }
 }
